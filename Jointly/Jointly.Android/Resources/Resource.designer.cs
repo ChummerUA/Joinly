8174992ab0aeecbd3c42fee43a21fd28327669fa
ChummerUA <chummer1008@gmail.com>
2019-05-23 14:00:17 +0300

    Update SetEventPageUI
    SetEventPage now change content orientation depending on device orientation
    Added icons for pins

--- Jointly/Jointly.Android/Resources/Resource.designer.cs
+++ Jointly/Jointly.Android/Resources/Resource.designer.cs
@@ -8498,26 +8498,26 @@ namespace Jointly.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f020153
-			public const int avd_hide_password_1 = 2130837843;
-			
-			// aapt resource value: 0x7f020154
-			public const int avd_hide_password_2 = 2130837844;
-			
 			// aapt resource value: 0x7f020155
-			public const int avd_hide_password_3 = 2130837845;
+			public const int avd_hide_password_1 = 2130837845;
+			
+			// aapt resource value: 0x7f020156
+			public const int avd_hide_password_2 = 2130837846;
+			
+			// aapt resource value: 0x7f020157
+			public const int avd_hide_password_3 = 2130837847;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f020156
-			public const int avd_show_password_1 = 2130837846;
-			
-			// aapt resource value: 0x7f020157
-			public const int avd_show_password_2 = 2130837847;
-			
 			// aapt resource value: 0x7f020158
-			public const int avd_show_password_3 = 2130837848;
+			public const int avd_show_password_1 = 2130837848;
+			
+			// aapt resource value: 0x7f020159
+			public const int avd_show_password_2 = 2130837849;
+			
+			// aapt resource value: 0x7f02015a
+			public const int avd_show_password_3 = 2130837850;
 			
 			// aapt resource value: 0x7f020056
 			public const int baseline_add_circle_black_36dp = 2130837590;
@@ -9260,11 +9260,11 @@ namespace Jointly.Droid
 			// aapt resource value: 0x7f02014c
 			public const int notification_icon_background = 2130837836;
 			
-			// aapt resource value: 0x7f020151
-			public const int notification_template_icon_bg = 2130837841;
+			// aapt resource value: 0x7f020153
+			public const int notification_template_icon_bg = 2130837843;
 			
-			// aapt resource value: 0x7f020152
-			public const int notification_template_icon_low_bg = 2130837842;
+			// aapt resource value: 0x7f020154
+			public const int notification_template_icon_low_bg = 2130837844;
 			
 			// aapt resource value: 0x7f02014d
 			public const int notification_tile_bg = 2130837837;
@@ -9273,10 +9273,16 @@ namespace Jointly.Droid
 			public const int notify_panel_notification_icon_bg = 2130837838;
 			
 			// aapt resource value: 0x7f02014f
-			public const int tooltip_frame_dark = 2130837839;
+			public const int pin_blue = 2130837839;
 			
 			// aapt resource value: 0x7f020150
-			public const int tooltip_frame_light = 2130837840;
+			public const int pin_green = 2130837840;
+			
+			// aapt resource value: 0x7f020151
+			public const int tooltip_frame_dark = 2130837841;
+			
+			// aapt resource value: 0x7f020152
+			public const int tooltip_frame_light = 2130837842;
 			
 			static Drawable()
 			{
--- Jointly/Jointly/Views/SetEventPage.xaml.cs
+++ Jointly/Jointly/Views/SetEventPage.xaml.cs
@@ -8,13 +8,18 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 using Xamarin.Forms.Xaml;
+using Xamarin.Essentials;
 
 namespace Jointly.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class SetEventPage : ContentPage
     {
-        private SetEventViewModel ViewModel { get; set; }
+        public SetEventViewModel ViewModel { get; set; }
+
+        public StackOrientation ContentOrientation { get; set; }
+
+        public double ContainerWidth { get; set; }
 
         public SetEventPage()
         {
@@ -22,13 +27,38 @@ namespace Jointly.Views
 
             ViewModel = new SetEventViewModel();
             BindingContext = ViewModel;
+
+            SetOrientation();
+
+            DeviceDisplay.MainDisplayInfoChanged += DeviceDisplay_MainDisplayInfoChanged;
+        }
+
+        private void DeviceDisplay_MainDisplayInfoChanged(object sender, DisplayInfoChangedEventArgs e)
+        {
+            SetOrientation();
+        }
+
+        private void SetOrientation()
+        {
+            var displayInfo = DeviceDisplay.MainDisplayInfo;
+            if (displayInfo.Orientation == DisplayOrientation.Portrait)
+            {
+                ContentOrientation = StackOrientation.Vertical;
+                ContainerWidth = displayInfo.Width;
+            }
+            else if (displayInfo.Orientation == DisplayOrientation.Landscape)
+            {
+                ContentOrientation = StackOrientation.Horizontal;
+                ContainerWidth = displayInfo.Width / 2;
+            }
+            OnPropertyChanged("ContentOrientation");
+            OnPropertyChanged("ContainerWidth");
         }
 
         protected override void OnAppearing()
         {
             var position = new Position(50.619900, 26.251617);
             Map.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromMeters(500)));
-            //EventPin.Position = new Position(50.619900, 26.251617);
             base.OnAppearing();
         }
     }
